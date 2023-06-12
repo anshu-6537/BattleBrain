04daf04e63fcf1412f80cbc0f47ccec6ee1a8316
anshu-6537 <pandeyanshumanofficial@gmail.com>
2023-06-13 00:45:12 +0530

    Added Splash screen

--- app/src/main/java/com/example/quizlingo/MainActivity.kt
+++ app/src/main/java/com/example/quizlingo/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.example.quizlingo
 
-import android.R.attr.button
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
-import org.w3c.dom.Text
+import pl.droidsonroids.gif.GifTextView
 
 
 class MainActivity : AppCompatActivity() {
-
 
 
     var BASE_URL = "https://opentdb.com/api.php"
@@ -31,8 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         val difficulty = intent.getStringExtra("diff")
         val number = intent.getStringExtra("num")
-        Toast.makeText(this,difficulty,Toast.LENGTH_SHORT).show()
-        Toast.makeText(this,number,Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this,difficulty,Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this,number,Toast.LENGTH_SHORT).show()
         quesnum = number?.toInt()!!
         if (difficulty != null) {
             fetchQuestion(quesnum, difficulty)
@@ -49,6 +46,8 @@ class MainActivity : AppCompatActivity() {
     ) {
 
         val requestQueue: RequestQueue = Volley.newRequestQueue(this)
+        val go_gif = findViewById<GifTextView>(R.id.go_gif)
+        val const = findViewById<ConstraintLayout>(R.id.constraint)
 
 
         val url = "$BASE_URL?amount=$amount&category=28&difficulty=$difficulty&type=multiple"
@@ -57,7 +56,14 @@ class MainActivity : AppCompatActivity() {
             Request.Method.GET, url, null,
             { response ->
                 val triviaQuestions = parseTriviaQuestions(response)
-                Toast.makeText(this, triviaQuestions[0].question, Toast.LENGTH_LONG).show()
+                val view: View = go_gif
+                view.postDelayed(
+                    {
+                        view.visibility = View.GONE
+                        const.visibility = View.VISIBLE
+                    },
+                    3000
+                )
                 displayQues(triviaQuestions)
             },
             { error ->
@@ -100,10 +106,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayQues(questionArray: List<Questions>) {
         val questext = findViewById<TextView>(R.id.questionTextView)
-        val ans1 = findViewById<TextView>(R.id.option1)
-        val ans2 = findViewById<TextView>(R.id.option2)
-        val ans3 = findViewById<TextView>(R.id.option3)
-        val ans4 = findViewById<TextView>(R.id.option4)
+//        val ans1 = findViewById<TextView>(R.id.option1)
+//        val ans2 = findViewById<TextView>(R.id.option2)
+//        val ans3 = findViewById<TextView>(R.id.option3)
+//        val ans4 = findViewById<TextView>(R.id.option4)
         val next = findViewById<Button>(R.id.nextButton)
         val radioButton1 = findViewById<RadioButton>(R.id.answerRadioButton1)
         val radioButton2 = findViewById<RadioButton>(R.id.answerRadioButton2)
@@ -111,90 +117,80 @@ class MainActivity : AppCompatActivity() {
         val radioButton4 = findViewById<RadioButton>(R.id.answerRadioButton4)
         val score = findViewById<TextView>(R.id.your_score)
         val your_score = findViewById<TextView>(R.id.score)
-        var i=0
+        val progress = findViewById<ProgressBar>(R.id.progressBar)
+        var i = 0
+        var userscore = 0
 
-        Toast.makeText(this, questionArray[0].correctanswer, Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, questionArray[0].correctanswer, Toast.LENGTH_SHORT).show()
 
         questext.text = questionArray[i].question
-        ans1.text = questionArray[i].incorrectanswer[1]
-        ans2.text = questionArray[i].incorrectanswer[0]
-        ans3.text = questionArray[i].correctanswer
-        ans4.text = questionArray[i].incorrectanswer[2]
+        radioButton1.text = questionArray[i].incorrectanswer[1]
+        radioButton2.text = questionArray[i].incorrectanswer[0]
+        radioButton3.text = questionArray[i].correctanswer
+        radioButton4.text = questionArray[i].incorrectanswer[2]
+        val timer = findViewById<TextView>(R.id.textView)
+
+        // time count down for 15 seconds,
+        // with 1 second as countDown interval
+        object : CountDownTimer(14000, 1000) {
+
+            // Callback function, fired on regular interval
+            override fun onTick(millisUntilFinished: Long) {
+                timer.setText("Time remaining: " + millisUntilFinished / 1000)
+            }
+
+            // Callback function, fired
+            // when the time is up
+            override fun onFinish() {
+                timer.setText("Times up !")
+
+            }
+        }.start()
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radiogroup);
+
+        next.setOnClickListener {
+            val selectedId = radioGroup.checkedRadioButtonId
+            val radioButton = findViewById<RadioButton>(selectedId)
+            if (radioButton.getText() == questionArray[i].correctanswer)
+                userscore += 1
+            radioGroup.clearCheck()
+        }
 
         next.setOnClickListener {
             i++
-            if (i<questionArray.size) {
+            if (i < questionArray.size) {
                 questext.text = questionArray[i].question
-                ans1.text = questionArray[i].incorrectanswer[1]
-                ans2.text = questionArray[i].incorrectanswer[0]
-                ans3.text = questionArray[i].correctanswer
-                ans4.text = questionArray[i].incorrectanswer[2]
-            }
-            else
-            {
+                radioButton1.text = questionArray[i].incorrectanswer[1]
+                radioButton2.text = questionArray[i].incorrectanswer[0]
+                radioButton3.text = questionArray[i].correctanswer
+                radioButton4.text = questionArray[i].incorrectanswer[2]
+
+                // get selected radio button from radioGroup
+                val selectedId = radioGroup.checkedRadioButtonId
+
+                // find the radiobutton by returned id
+                val radioButton = findViewById<RadioButton>(selectedId)
+                if (radioButton.getText() == questionArray[i].correctanswer)
+                    userscore += 1
+                radioGroup.clearCheck()
+            } else {
                 radioButton1.visibility = View.INVISIBLE
                 radioButton2.visibility = View.INVISIBLE
                 radioButton3.visibility = View.INVISIBLE
                 radioButton4.visibility = View.INVISIBLE
                 questext.visibility = View.INVISIBLE
-                ans1.visibility = View.INVISIBLE
-                ans2.visibility = View.INVISIBLE
-                ans3.visibility = View.INVISIBLE
-                ans4.visibility = View.INVISIBLE
+
                 next.visibility = View.INVISIBLE
                 score.visibility = View.VISIBLE
                 your_score.visibility = View.VISIBLE
+                progress.visibility = View.VISIBLE
+                your_score.text = userscore.toString()
+
             }
         }
     }
 }
 
-//        var i=0
-//        while (i<questionArray.size) {
-//            val ques = questionArray[i].question
-//            val correct = questionArray[i].correctanswer
-//            val incorrect1 = questionArray[i].incorrectanswer[0]
-//            val incorrect2 = questionArray[i].incorrectanswer[1]
-//            val incorrect3 = questionArray[i].incorrectanswer[2]
-//            questext.text = ques
-//            ans1.text = incorrect2
-//            ans2.text = incorrect3
-//            ans3.text = correct
-//            ans4.text = incorrect1
-//
-//            //check next is pressed or not
-//            var clicked = false
-//
-//            //my button clic
-//
-//            //my button clic
-//            next.setOnClickListener { //change boolean value
-//                clicked = true
-//                if (clicked) {
-//                    i += 1
-//                }
-//            }
-//        }
-//        for (i in questionArray.indices)
-//        {
-//            val ques = questionArray[i].question
-//            val correct = questionArray[i].correctanswer
-//            val incorrect1 = questionArray[i].incorrectanswer[0]
-//            val incorrect2 = questionArray[i].incorrectanswer[1]
-//            val incorrect3 = questionArray[i].incorrectanswer[2]
-//
-//            questext.text = ques
-//            ans1.text = incorrect1
-//            ans2.text = incorrect2
-//            ans3.text = correct
-//            ans4.text = incorrect3
-//
-//        }
 
-//        questext.text = "Quiz Completed"
-//        ans1.isEnabled = false
-//        ans2.isEnabled = false
-//        ans3.isEnabled = false
-//        ans4.isEnabled = false
-//        next.visibility = View.INVISIBLE
 
